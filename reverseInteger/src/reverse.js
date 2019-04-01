add2188276bf2f0444cb562dc1f98670b70916a9
R5D4 <ding.randy@gmail.com>
2019-03-31 22:42:08 -0400

    reverseInteger: passed all tests

--- reverseInteger/src/reverse.js
+++ reverseInteger/src/reverse.js
@@ -1,5 +1,9 @@
 function reverse (x) {
-
+    let reverseStr = Math.abs(x).toString().split('');
+    if ( x < 0 ) { reverseStr.push('-') };
+    let num = +(reverseStr.reverse().join(''));
+    if (num > 2**31 - 1 || num < -1 * 2**31) { return 0 };
+    return num;
 }
 
 exports.reverse = reverse;
--- reverseInteger/test/test.js
+++ reverseInteger/test/test.js
@@ -4,7 +4,13 @@ const { reverse } = require('../src/reverse');
 describe('reverse', () => {
     it('should return 321 for input 123', () => {
         const x = 123;
-        const reversed = reverse(123);
+        const reversed = reverse(x);
         expect(reversed).to.equal(321);
+    });
+
+    it('should return -321 for input -123', () => {
+        const x = -123;
+        const reversed = reverse(x);
+        expect(reversed).to.equal(-321);
     });
 });
